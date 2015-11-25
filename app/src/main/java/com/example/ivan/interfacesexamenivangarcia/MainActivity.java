78f604c806824829c4d43a216a08fa2f9ba06204
ivan <ivangarfdez@gmail.com>
2015-11-25 10:46:14 +0100

    Examen Acabado

--- app/src/main/java/com/example/ivan/interfacesexamenivangarcia/MainActivity.java
+++ app/src/main/java/com/example/ivan/interfacesexamenivangarcia/MainActivity.java
@@ -19,8 +19,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Declaramos todos los views que vamos a usar
